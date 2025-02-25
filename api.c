9315541f554bd2aa91f79cce2cb34fe2c35f964c
Olaf1001 <olaf1001.front@gmail.com>
2025-02-25 11:15:49 +0100

    Add base file structure

---
+++ api.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "api.h"
+
+void send_query(char *prompt) {
+    printf("send_query function\n");
+}
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "api.h"
+
+int main() {
+    send_query("test");
+
+    return 0;
+}
